9b16e12e9dee0e5c18bdf5352de908b2f3af22ef
Vishnu Kumar Jangid <vkjangid8104@gmail.com>
2026-03-17 16:25:22 +0530

    updated code

---
+++ backend/routes/auth.js
@@ -0,0 +1,51 @@
+// const express = require("express");
+
+// const router = express.Router();
+
+// const {
+//   sendOtp,
+//   verifyOtp,
+//   getUser,
+//   getSchools,
+//   submitSchools,
+// } = require("../controllers/authController");
+
+// const { verifyToken } = require("../middleware/verifyToken");
+
+// // OTP routes
+// router.post("/send-otp", sendOtp);
+// router.post("/verify-otp", verifyOtp);
+
+// // Protected routes
+// router.get("/user/:id", verifyToken, getUser);
+// router.get("/schools", verifyToken, getSchools);
+// router.post("/submit-schools", verifyToken, submitSchools);
+
+// module.exports = router;
+
+const express = require("express");
+const router = express.Router();
+
+const {
+  sendOtp,
+  verifyOtp,
+  getUser,
+  getSchools,
+  submitSchools,
+  submitFinalForm
+} = require("../controllers/authController");
+
+const { verifyToken } = require("../middleware/verifyToken");
+
+router.post("/send-otp", sendOtp);
+router.post("/verify-otp", verifyOtp);
+
+router.get("/user/:id", verifyToken, getUser);
+
+router.get("/schools", verifyToken, getSchools);
+
+router.post("/submit-schools", verifyToken, submitSchools);
+
+router.post("/submit-form", verifyToken, submitFinalForm);
+
+module.exports = router;
