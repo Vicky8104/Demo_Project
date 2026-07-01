49e2e8b895649eb974fae5b07cbf452ba376bd2a
Vicky8104 <vkjangid8104@gmail.com>
2026-07-01 23:09:20 +0530

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,18 +1,11 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import dotenv from "dotenv";
+import admin from "firebase-admin";
 
-dotenv.config();
+if (!admin.apps.length) {
+admin.initializeApp({
+credential: admin.credential.cert(
+JSON.parse(process.env.FIREBASE_ADMIN_KEY)
+)
+});
+}
 
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-};
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
+export default admin;
