411003f07fb64a332409b1487b6712de55040521
Vicky8104 <vkjangid8104@gmail.com>
2026-07-01 23:55:45 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import admin from "firebase-admin";
 
 if (!admin.apps.length) {
   admin.initializeApp({
