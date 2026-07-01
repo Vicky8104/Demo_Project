9b2967d52072678a8c5007978697440ba6b93c19
Vicky8104 <vkjangid8104@gmail.com>
2026-07-01 23:03:39 +0530

    Update firebase.js

--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,21 +1,3 @@
-// firebase.js
-// import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyCpD73HZZMSNVZ-2TAfQVR83iGPJ5yOloQ",
-//   authDomain: "teachercounselingproject.firebaseapp.com",
-//   projectId: "teachercounselingproject",
-//   storageBucket: "teachercounselingproject.firebasestorage.app",
-//   messagingSenderId: "984891191013",
-//   appId: "1:984891191013:web:72a9cecc425c07f531ee0c"
-// };
-
-// const app = initializeApp(firebaseConfig);
-
-// export const auth = getAuth(app);
-
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import dotenv from "dotenv";
