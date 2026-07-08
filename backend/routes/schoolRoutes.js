621f732078e67c174bb593db012d1e526dd97b83
Vicky8104 <vkjangid8104@gmail.com>
2026-07-08 21:59:30 +0530

    Update schoolRoutes.js

--- backend/routes/schoolRoutes.js
+++ backend/routes/schoolRoutes.js
@@ -1,26 +1,3 @@
-// // routes/schoolRoutes.js
-// import express from "express";
-// import School from "../models/School.js";
-
-// const router = express.Router();
-
-// // GET /api/schools?post=&area=&subject=
-// router.get("/", async (req, res) => {
-//   try {
-//     const { post, area, subject } = req.query;
-
-//     const schools = await School.find({
-//       post,
-//       area,
-//       subject
-//     });
-
-//     res.json(schools);
-
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
 
 // export default router;
 import express from "express";
