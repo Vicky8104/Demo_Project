576ab3b27d76e2a0330db22d779b09a3508ffe00
Vicky8104 <vkjangid8104@gmail.com>
2026-07-08 21:51:17 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,7 +1,12 @@
 import jwt from "jsonwebtoken";
 
 export const protect = (req, res, next) => {
-  const token = req.cookies.token;
+  // const token = req.cookies.token;
+   const token =
+    req.cookies.token ||
+    req.headers.authorization?.split(" ")[1];
+
+   
 
   if (!token) {
     return res.status(401).json({ message: "Not authorized" });
