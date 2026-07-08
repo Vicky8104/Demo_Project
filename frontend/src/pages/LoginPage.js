2c5fee5e2d1f3d5793af6c07ff28e33af6fd14c1
Vicky8104 <vkjangid8104@gmail.com>
2026-07-08 06:40:59 +0530

    Update LoginPage.js

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -14,9 +14,6 @@ export default function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
 
   // ================= LOGIN =================
