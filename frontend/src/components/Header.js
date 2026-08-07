5bc4510c6ca93651fa7b61ce8a451feb2c34391e
Vicky8104 <vkjangid8104@gmail.com>
2026-08-07 12:44:58 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -68,13 +68,7 @@ export default function Header({ toggleSidebar }) {
           <h2>पदस्थापन-परामर्श-पोर्टलम्</h2>
         </div> */}
 
-          <div className="left">
      
-          <div className="info">
-            <h4>{members[index].name}</h4>
-            <p>{members[index].post}</p>
-          </div>
-        </div>
 
       </div>
 
