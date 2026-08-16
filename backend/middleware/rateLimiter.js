903e77a382c843bc99cf5fe4e7ad9b139a06894b
Vicky8104 <vkjangid8104@gmail.com>
2026-08-16 12:41:35 +0530

    Update rateLimiter.js

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,15 +1,36 @@
 import rateLimit, { ipKeyGenerator } from "express-rate-limit";
 
+// export const globalLimiter = rateLimit({
+//     windowMs: 15*60*1000,
+//     max:1000,
+//     message:"Too many requests, try later"
+// });
 export const globalLimiter = rateLimit({
-    windowMs: 15*60*1000,
-    max:100,
-    message:"Too many requests, try later"
+  windowMs: 15 * 60 * 1000,
+  max: 1000,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many requests. Please try again later."
+  }
 });
 
+// export const loginLimiter = rateLimit({
+//     windowMs: 10*60*1000,
+//     max:100,
+//     message:"Too many Login attempts."
+// });
+
 export const loginLimiter = rateLimit({
-    windowMs: 10*60*1000,
-    max:100,
-    message:"Too many Login attempts."
+  windowMs: 10 * 60 * 1000,
+  max: 20,
+  standardHeaders: true,
+  legacyHeaders: false,
+  message: {
+    success: false,
+    message: "Too many login attempts. Please try again later."
+  }
 });
 
 export const otpLimiter = rateLimit({
