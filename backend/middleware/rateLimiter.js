f6e908056487c27a398d2c86221fa554f2b96702
Vicky8104 <vkjangid8104@gmail.com>
2026-08-17 15:07:37 +0530

    Increase max login attempts to 100

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -24,7 +24,7 @@ export const globalLimiter = rateLimit({
 
 export const loginLimiter = rateLimit({
   windowMs: 10 * 60 * 1000,
-  max: 20,
+  max: 100,
   standardHeaders: true,
   legacyHeaders: false,
   message: {
